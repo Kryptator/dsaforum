336e236047f9cd6aa9e0b98bd66ceb60528fd29b
Kryptator <zornbold@gmail.com>
2017-06-07 06:54:13 +0200

    Create functions_display.php
    Neue Beiträge Markierung in Forum verhindern, wenn es einen neuen Beitrag in einem Subforum gibt

---
+++ functions_display.php
@@ -0,0 +1,10 @@
+++ finde ++
+
+// If one subforum is unread the forum gets unread too...
+
+++ Entferne oder kommentiere aus ++
+
+			if ($subforum_unread)
+				{
+					$forum_unread = true;
+				}
