a241b249c9c42983ca548684418a0c54edc14c4b
Kryptator <zornbold@gmail.com>
2017-06-07 08:12:34 +0200

    Create functions_display.php

---
+++ includes/functions_display.php
@@ -0,0 +1,11 @@
+// Neue Markierung von überliegenden Foren verhindern, wenn ein neuer post in einem Subforum vorhanden ist.
+++ finde ++
+
+// If one subforum is unread the forum gets unread too...
+
+++ Entferne oder kommentiere aus ++
+
+			if ($subforum_unread)
+				{
+					$forum_unread = true;
+				}
